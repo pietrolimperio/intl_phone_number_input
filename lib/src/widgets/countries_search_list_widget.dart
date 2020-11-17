b81fe21400e0c9fe8283bf0f6d9e5f21f5b57c83
pietrolimperio <37225931+pietrolimperio@users.noreply.github.com>
2020-11-17 22:41:44 +0100

    Update countries_search_list_widget.dart

--- lib/src/widgets/countries_search_list_widget.dart
+++ lib/src/widgets/countries_search_list_widget.dart
@@ -46,7 +46,7 @@ class _CountrySearchListWidgetState extends State<CountrySearchListWidget> {
 
   InputDecoration getSearchBoxDecoration() {
     return widget.searchBoxDecoration ??
-        InputDecoration(labelText: label);
+        InputDecoration(labelText: widget.label);
   }
 
   List<Country> filterCountries() {
